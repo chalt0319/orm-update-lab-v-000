3988c4faa42f91eddaa2b7aba22246fcb3336300
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-18 10:50:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,7 +66,7 @@ attr_reader :id
     SQL
 
     row = DB[:conn].execute(sql, name)
-    new_student = Student.new_from_db(row)
+    new_student = Student.new_from_db(row.flatten)
     new_student
   end
 
